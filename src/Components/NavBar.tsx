5800cf8d4cd2ed3594ceb0b63fc172e4dca6abff
NeeloByron <byronnkhuna@gmail.com>
2026-07-03 15:47:15 +0200

    Added NavBar Components

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div>
+        <a href='#Home'>Home</a>
+        <a href='#About-Us'>About Us</a>
+        <a href='#Services'>Services</a>
+        <a href='#Page'>Page</a>
+        <a href='#Contact-Us'>Contact Us</a>
+    </div>
   )
 }
