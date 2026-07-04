55cba5996d421a96f683facac18121f297ffdac4
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-07-04 21:31:47 +0200

    Navheader background styling & replaced logo

--- src/Component/NavBar.tsx
+++ src/Component/NavBar.tsx
@@ -3,10 +3,11 @@ import './NavBar.css'
 function NavBar () {
   return (
     
-    <header className="Navheader">
-     <div className="logo">
-      <img src="/src/assets/logo.png" alt="logo" />
+  <header className="Navheader">
+    <div className="logo">
+      <img src="/src/assets/akasa-air.svg" alt="akasa-air" />
     </div>
+
     <div className="navbar">
       <nav className="links">
         <a href="/home">HOME</a>
@@ -14,7 +15,7 @@ function NavBar () {
         <a href="/pages">PAGES</a>
         <a href="/blog">BLOG</a>
         <a href="/contact">CONTACT US</a>
-       </nav>
+      </nav>
 
        <div className="navIcons">
         <button className='iconBtn'>👤</button>
