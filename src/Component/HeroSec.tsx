8d335d879e1d715b3566cd32d7590d819f17582b
NeeloByron <byronnkhuna@gmail.com>
2026-07-06 11:09:48 +0200

    Text for the hero section

--- src/Component/HeroSec.tsx
+++ src/Component/HeroSec.tsx
@@ -6,9 +6,13 @@ export const HeroSec = () => {
     <>
       <div className='backgroundImg'>
         <img src={HeroImg} alt='' />
-        <h3>Prelum Restraurant</h3>
-        <h1>Anida Dedelay</h1>
-        <p>BEST HEALTHY SALAD SERVED IN OUR RESTRAURANT</p>
+
+         <div className='text'>
+           <h3>Prelum Restraurant</h3>
+           <h1>Anida Dedelay</h1>
+           <p>BEST HEALTHY SALAD SERVED IN OUR RESTRAURANT</p>
+         </div>
+
       </div>
 
    </> );
