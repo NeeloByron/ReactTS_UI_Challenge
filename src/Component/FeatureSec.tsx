2cd1a126df0f80882f2677b266d45488bebe8c3c
NeeloByron <byronnkhuna@gmail.com>
2026-07-07 09:24:24 +0200

    Update on the styling for learn more button

--- src/Component/FeatureSec.tsx
+++ src/Component/FeatureSec.tsx
@@ -9,7 +9,7 @@ export const FeatureSec = () => {
                 <h1 className='benefits'>Orange Benefit</h1>
                 <p className='text'>Just reogine eseds and summer in golled quasiunp</p>
                 <img src='/src/assets/Orange.png' alt=''/>
-                <a href='learnMore'>LEARN MORE</a>
+                <a href='learnMore' className='LearnMore'>LEARN MORE</a>
             </div>
 
              <div className='FeatureSecOrangeGlass'>
