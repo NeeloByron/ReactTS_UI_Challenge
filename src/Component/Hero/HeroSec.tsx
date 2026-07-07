00c739a4daad4ebbb8c226936fddad8f283db671
NeeloByron <byronnkhuna@gmail.com>
2026-07-07 15:26:58 +0200

    Update

--- src/Component/Hero/HeroSec.tsx
+++ src/Component/Hero/HeroSec.tsx
@@ -1,16 +1,21 @@
-import style from './HeroSec.css';
+import './HeroSec.css';
 
 export const HeroSec = () => {
   return (
     <>
       <div className='backgroundImg'>
        
-         <div className='text'>
-           <h3>Prelum Restraurant</h3>
-           <h1>Anida Dedelay</h1>
-           <p>BEST HEALTHY SALAD SERVED IN OUR RESTRAURANT</p>
-           <a href='learnmore' className={style.learnmore}>LEARN MORE</a>
-         </div>
+           <div className='text'>
+              <h3>Prelum Restraurant</h3>
+              <h1>Anida Dedelay</h1>
+              <p>BEST HEALTHY SALAD SERVED IN OUR RESTRAURANT</p>
+              <a href='learnmore' className='learnmore'>LEARN MORE</a>
+           </div>
+          
+          <div className='signup'>
+              <a href='signingup' className='signUp'>SIGN UP</a>
+          </div>
+
 
       </div>
 
