d90a8ae66dec3e428cca4c278badc2a33983499c
NeeloByron <byronnkhuna@gmail.com>
2026-07-09 15:49:16 +0200

    Modified Menu in a simplifed way for my css

--- src/Component/Menu/MenuSec.tsx
+++ src/Component/Menu/MenuSec.tsx
@@ -1,6 +1,6 @@
 import MenuImage1 from '@/assets/ARCU.png'
 import MenuImage2 from '@/assets/Orange2.png'
-import MenuImage3 from '@/assets/StrewBerryBowl.jpg'
+import MenuImage3 from '@/assets/StrewBerryBowl.png'
 import './MenuSec.css'
 
 export const MenuSec = () => {
@@ -8,22 +8,22 @@ export const MenuSec = () => {
     <>
       <div className="Main-Menu">
 
-           <div className="Menu-card1">
-             <img src={MenuImage1} alt='MenuIamge-1' />
-             <h3 className='Menu-card1-head'>ARCU VOLUT VITAE</h3>
-             <p className='Menu-card1-text'>Phestiers lgeart</p>
+           <div className="Menu-card">
+             <img src={MenuImage1} alt='MenuIamge' />
+             <h3 className='Menu-card-head'>ARCU VOLUT VITAE</h3>
+             <p className='Menu-card-text'>Phestiers lgeart</p>
            </div>
        
-           <div className='Menu-card2'>
-             <img src={MenuImage2} alt='MenuIamge-2' />
-             <h3 className='Menu-card2-head'>$19,99,90,99E</h3>
-             <p className='Menu-card2-text'>Orviste past bronps</p>
+           <div className='Menu-card'>
+             <img src={MenuImage2} alt='MenuIamge' />
+             <h3 className='Menu-card-head'>$19,99,90,99E</h3>
+             <p className='Menu-card-text'>Orviste past bronps</p>
            </div>
 
-           <div className='Menu-card3'>
-             <img src={MenuImage3} alt='MenuIamge-3' />
-             <h3 className='Menu-card3-head'>SFIELY TUN DRENARD</h3>
-             <p className='Menu-card3-text'>Ongand suniers</p>
+           <div className='Menu-card'>
+             <img src={MenuImage3} alt='MenuIamge' />
+             <h3 className='Menu-card-head'>SFIELY TUN DRENARD</h3>
+             <p className='Menu-card-text'>Ongand suniers</p>
            </div>
 
       </div>
