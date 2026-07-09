bd8a783665d466b3286fc34160cbbc738cff5fb3
NeeloByron <byronnkhuna@gmail.com>
2026-07-09 08:22:24 +0200

    Update naming for images in the MenuSection

--- src/Component/Menu/MenuSec.tsx
+++ src/Component/Menu/MenuSec.tsx
@@ -3,21 +3,21 @@ import './MenuSec.css'
 export const MenuSec = () => {
   return (
     <>
-      <div className="MenuSec">
+      <div className="Main-Menu">
 
-          <div className="Menu1">
+          <div className="Menu-card1">
             <img src='src/assets/ARCU.png' alt='' />
             <h3>ARCU VOLUT VITAE</h3>
              Phestiers lgeart
           </div>
        
-          <div className='Menu2'>
+          <div className='Menu-card2'>
             <img src='src/assets/Orange2.png' alt='' />
             <h3>$19,99,90,99E</h3>
              Orviste past bronps
           </div>
 
-          <div className='Menu3'>
+          <div className='Menu-card3'>
             <img src='src/assets/StrewBerryBowl.png' alt='' />
             <h3>SFIELY TUN DRENARD</h3>
              Ongand suniers
