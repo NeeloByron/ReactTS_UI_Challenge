03bde96065c06ad299e5670a4fa5eeef36b70a48
NeeloByron <byronnkhuna@gmail.com>
2026-07-09 11:56:48 +0200

    Changed images & styling

--- src/Component/Menu/MenuSec.tsx
+++ src/Component/Menu/MenuSec.tsx
@@ -1,3 +1,6 @@
+import MenuImage1 from '../../assets/ARCU.png'
+import MenuImage2 from '../../assets/Orange2.png'
+import MenuImage3 from '../../assets/StrewBerryBowl.jpg'
 import './MenuSec.css'
 
 export const MenuSec = () => {
@@ -5,23 +8,23 @@ export const MenuSec = () => {
     <>
       <div className="Main-Menu">
 
-          <div className="Menu-card1">
-            <img src='src/assets/ARCU.png' alt='' />
-            <h3>ARCU VOLUT VITAE</h3>
-             Phestiers lgeart
-          </div>
+           <div className="Menu-card1">
+             <img src={MenuImage1} alt='MenuIamge-1' />
+             <h3 className='Menu-card1-head'>ARCU VOLUT VITAE</h3>
+             <p className='Menu-card1-text'>Phestiers lgeart</p>
+           </div>
        
-          <div className='Menu-card2'>
-            <img src='src/assets/Orange2.png' alt='' />
-            <h3>$19,99,90,99E</h3>
-             Orviste past bronps
-          </div>
+           <div className='Menu-card2'>
+             <img src={MenuImage2} alt='MenuIamge-2' />
+             <h3 className='Menu-card2-head'>$19,99,90,99E</h3>
+             <p className='Menu-card2-text'>Orviste past bronps</p>
+           </div>
 
-          <div className='Menu-card3'>
-            <img src='src/assets/StrewBerryBowl.png' alt='' />
-            <h3>SFIELY TUN DRENARD</h3>
-             Ongand suniers
-          </div>
+           <div className='Menu-card3'>
+             <img src={MenuImage3} alt='MenuIamge-3' />
+             <h3 className='Menu-card3-head'>SFIELY TUN DRENARD</h3>
+             <p className='Menu-card3-text'>Ongand suniers</p>
+           </div>
 
       </div>
     </>
