e74967358f8cfeeaca7a3e42486a43537b057321
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-07-11 22:21:01 +0200

    Added footer card image and styling

--- src/Component/Footer/FooterSec.tsx
+++ src/Component/Footer/FooterSec.tsx
@@ -1,10 +1,26 @@
+import footerImage from '@/assets/BerryBowlfooter.png'
 import './Footer.Module.css'
 
 export const FooterSec = () => {
   return (
     <>
       <div className='footerMain'>
-      
+         
+          <div className='imageCard'>
+            <img src={footerImage} />
+          </div>
+
+          <div className='info'>
+            <h1 className='infoHeader'>Our Menu Benefit</h1>
+            
+            <div className='freshImg'>
+              
+              <h1>Fresh</h1>
+              <p>Plays orand on inorianties</p>
+            </div>
+
+
+         </div>
       </div>
     </>
   )
