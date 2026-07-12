3f41f6024dd3935b6fefd8c2ae110b404bfd70d6
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-07-12 21:36:04 +0200

    Updates on the navbar

--- src/Component/NavBar/NavBar.tsx
+++ src/Component/NavBar/NavBar.tsx
@@ -1,5 +1,8 @@
 import { Link } from 'react-router-dom';
-import './NavBar.css';
+import logoImg from '@/assets/logo.png'
+import firstICon from '@/assets/location.png'
+import shopCart from '@/assets/cart-shopping.png'
+import './NavBar.Module.css'
 
 function NavBar () {
   const navLinks = [
@@ -10,32 +13,39 @@ function NavBar () {
     { path: '/contactUs', label: 'CONTACT US'}
   ]
   return (
-  <header className="nav-header">
-    <div className="logo">
-      <img src="/src/assets/logo.png" alt="bower-logo" />
-    </div>
+    <>
+      {/*Main container of the navigation bar*/}
+      <header className="nav-header">
+        
+        {/*Logo class*/}
+        <div className="logo">
+          <img src={logoImg} alt="bower-logo" />
+        </div>
 
-    <nav className="navbar" role="navigation" aria-label="Main Navigation">
-      <ul className='nav-links'>
-         {navLinks.map((link) => (
-           <li key={link.path}>
-             <Link to={link.path}>{link.label}</Link>
-           </li>
-        ))}
-      </ul>
+        {/*nagivation bar map in list item*/}
+         <nav className="navbar" role="navigation" aria-label="Main Navigation">
+           <ul className='nav-links'>
+               {navLinks.map((link) => (
+                 <li key={link.path}>
+                   <Link to={link.path}>{link.label}</Link>
+                </li>
+                 ))}
+            </ul>
 
-     <div className="nav-icons">
-         <button className='locationBtn' aria-label="location">
-           <img src="/src/assets/location.png" alt="user-logo" className='ProfileImg'/>
-         </button>
-
-         <button className='ShoppingCartBtn' aria-label="Shopping Cart">
-           <img src="/src/assets/cart-shopping.png" alt="shopping-cart" className='CartImg'/>
+          {/*first icon either location or profile is not specified*/}
+          <div className="nav-icons">
+           <button className='locationBtn' aria-label="location">
+             <img src={firstICon} alt="user-logo" className='ProfileImg'/>
+           </button>
+          
+           {/*Shopping cart icon*/}
+           <button className='ShoppingCartBtn' aria-label="Shopping Cart">
+           <img src={shopCart} alt="shopping-cart" className='CartImg'/>
          </button>
       </div>
    </nav>
  </header>
-
+</>
   ) 
 }
 
