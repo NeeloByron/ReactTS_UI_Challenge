6cf11c55377014f6acd6872701e8640a4bd55508
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-07-12 03:15:07 +0200

    Updated my footer styling

--- src/Component/Footer/FooterSec.tsx
+++ src/Component/Footer/FooterSec.tsx
@@ -4,15 +4,17 @@ import './Footer.Module.css'
 export const FooterSec = () => {
   return (
     <>
+      {/*Main Footer*/}
       <div className='footerMain'>
          
           <div className='imageCard'>
             <img src={footerImage} />
           </div>
 
+          {/*Our menu benefit section*/}
           <div className='info'>
                <div className='infoHeader'>
-                  <h1>Our Menu Benefit</h1>
+                  <h1>OUR MENU BENEFIT</h1>
                </div>
            
            <div className='container'>
@@ -30,9 +32,14 @@ export const FooterSec = () => {
                   <h1>ORE ANIC</h1>
                   <p>Towagoloes weruis</p>
                </div>
-
             </div>
+
           </div>
+
+           {/*Opening hours*/}
+           <div className='OperationHours'>
+             <h1>OPENING HOURS</h1>
+           </div>
 
       </div>
     </>
