73b3d77a095f60947476cd6762a10fb76ae885e7
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-07-12 12:39:24 +0200

    Changed images for menu section and apply styling

--- src/Component/Menu/MenuSec.tsx
+++ src/Component/Menu/MenuSec.tsx
@@ -1,30 +1,34 @@
-import MenuImage1 from '@/assets/ARCU.png'
-import MenuImage2 from '@/assets/Orange2.png'
+import MenuImage1 from '@/assets/Orange1.jpg'
+import MenuImage2 from '@/assets/Orange2.jpg'
 import MenuImage3 from '@/assets/StrewBerryBowl.png'
-import './MenuSec.css'
+import './menuSec.Module.css'
 
 export const MenuSec = () => {
   return (
     <>
+      {/*main menu section*/}
       <div className="Main-Menu">
 
-           <div className="Menu-card">
-             <img src={MenuImage1} alt='MenuIamge' />
-             <h3 className='Menu-card-head'>ARCU VOLUT VITAE</h3>
-             <p className='Menu-card-text'>Phestiers lgeart</p>
-           </div>
-       
-           <div className='Menu-card'>
-             <img src={MenuImage2} alt='MenuIamge' />
-             <h3 className='Menu-card-head'>$19,99,90,99E</h3>
-             <p className='Menu-card-text'>Orviste past bronps</p>
-           </div>
-
-           <div className='Menu-card'>
-             <img src={MenuImage3} alt='MenuIamge' />
-             <h3 className='Menu-card-head'>SFIELY TUN DRENARD</h3>
-             <p className='Menu-card-text'>Ongand suniers</p>
-           </div>
+            {/*Menu 1*/}
+            <div className="Menu-card">
+              <img src={MenuImage1} alt='MenuIamge' />
+              <h3 className='Menu-card-head'>ARCU VOLUT VITAE</h3>
+              <p className='Menu-card-text'>Phestiers lgeart</p>
+            </div>
+            
+            {/*Menu 2*/}
+            <div className='Menu-card'>
+              <img src={MenuImage2} alt='MenuIamge' />
+              <h3 className='Menu-card-head'>$19,99,90,99E</h3>
+              <p className='Menu-card-text'>Orviste past bronps</p>
+            </div>
+            
+            {/*Menu 3*/}
+            <div className='Menu-card'>
+              <img src={MenuImage3} alt='MenuIamge' />
+              <h3 className='Menu-card-head'>SFIELY TUN DRENARD</h3>
+              <p className='Menu-card-text'>Ongand suniers</p>
+            </div>
 
       </div>
     </>
