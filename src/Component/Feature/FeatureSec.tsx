fc2adae04120c05823e2a43af6fc06605f9604bc
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-07-12 15:31:01 +0200

    Updated featureSec with comments and changed the styling with using the new class  values & comment on index css

--- src/Component/Feature/FeatureSec.tsx
+++ src/Component/Feature/FeatureSec.tsx
@@ -1,26 +1,40 @@
-import './FeartureSec.css';
+import SliceOrange from '@/assets/Orange.png'
+import OrangeContainer from '@/assets/OrangeGlass.png'
+import './FeartureSec.Module.css'
 
 export const FeatureSec = () => {
   return (
-    <>
-          <div className='FeatureSecImg'>
+    <> 
+       {/*Main feacture section*/}
+          <div className='mainFeature'>
+ 
+            {/*Sub main for the orange benefits part*/}
+             <div className='subMainFeature'>
 
-             <div className='FeatureSecText'>
-                <h1 className='benefits'>Orange Benefit</h1>
-                  <img src='/src/assets/Orange.png' alt=''/>
-                  <div className='MainText'>
-                     <p className='text'>Just reogine eseds and summer in golled quasiunp</p>
-                     <a href='learnMore' className='LearnMore'>LEARN MORE</a>
+                 {/*header*/}
+                 <div className='SubHeader'>
+                  <h1>Orange Benefit</h1>
+                 </div>
+             
+                 {/*Image for the half orange*/}
+                  <div className='orangeImage'>
+                    <img src={SliceOrange} alt='halfSlicedOrange'/>
                   </div>
-                
+ 
+                 {/*Paragraph message*/}
+                  <div className='MainText'>
+                     <p>Just reogine eseds and summer in golled quasiunp</p>
+                  </div>
+
+               </div>  
+
+               {/*Image of orange juice*/}
+                <div className='FeatureSecOrangeGlass'>
+                  <img src={OrangeContainer} alt='OrangeJuiceGlass' />
+                </div>
+
             </div>
-
-             <div className='FeatureSecOrangeGlass'>
-              <img src='/src/assets/OrangeGlass.png' alt='' />
-             </div>
                 
-
-          </div>
     </>
   )
 }
