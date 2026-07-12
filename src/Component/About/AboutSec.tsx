9c8dd4bf920b260f976a234ea377cfa65390cc8a
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-07-12 09:14:30 +0200

    Positioning image and cardbox classes

--- src/Component/About/AboutSec.tsx
+++ src/Component/About/AboutSec.tsx
@@ -4,18 +4,29 @@ import './aboutSec.Module.css'
 export const AboutSec = () => {
   return (
      <>
+        {/*Main section*/}
         <div className='aboutSection'> 
           <div className='aboutText'>
              <p>Just imagine seeds and summer in gell sunshine.</p>
            </div>
+           
+           {/*contains the two cards*/}
+           <div className='containerCard'>
 
-           <div className='aboutImageCard'>
-             <img src={strewBerryImg} alt="Strewberries" />
-           </div>
+             {/*Image for the middle card holding strewberries*/}
+              <div className='aboutImageCard'>
+                <img src={strewBerryImg} alt="Strewberries" />
+              </div>
 
-          <div className='cardBox'>
-            
-          </div>    
+              {/*the which background card*/}
+             <div className='cardBox'>
+               <h1>EBEE WOUS BENEFIT.</h1>
+               <p></p>
+             </div>   
+              
+
+          </div> 
+
       </div>
     </>
       )
