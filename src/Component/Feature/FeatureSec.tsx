53e33aa17dfdc5f86de723754f2177906c8400d7
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-07-13 23:13:07 +0200

    Added style to my feature section and media query for tablets

--- src/Component/Feature/FeatureSec.tsx
+++ src/Component/Feature/FeatureSec.tsx
@@ -6,43 +6,43 @@ import './FeartureSec.Module.css'
 export const FeatureSec = () => {
   return (
     <> 
-    {/*Main feacture section*/}
-      <div className='mainFeature'>
+      {/*Main feacture section*/}
+       <div className='mainFeature'>
  
-        {/*Sub main for the orange benefits part*/}
-          <div className='subMainFeature'>
+          {/*Sub main for the orange benefits part*/}
+           <div className='subMainFeature'>
 
-            {/*header*/}
+              {/*header*/}
               <div className='SubHeader'>
            
-                  <div className='OrangeContainer'>
-                    <h1>Orange Benefit</h1>
-                   </div>
+                <div className='OrangeContainer'>
+                  <h1>Orange Benefit</h1>
+                </div>
                   
-                   <div className='orangeAndText'>
-
-                     {/*Image for the half orange*/}
+                  <div className='orangeAndText'>
+                      {/*Image for the half orange*/}
                       <div className='orangeImage'>
-                        <img src={SliceOrange} alt='halfSlicedOrange'/>
+                        <img src={SliceOrange} />
                       </div>
                   
-                     {/*Paragraph message*/}
+                      {/*Paragraph message*/}
                       <div className='MainText'>
                         <p>Just reogine eseds and summer in golled quasiunp</p>
-                        <Button btnText='LEARN MORE' style={{width: '14rem', height: '5.5rem', fontSize: '18px'}}/>
+                        <Button btnText='LEARN MORE' style={{width: '12rem', height: '4rem'}}/>
                       </div>
 
                   </div>
 
-               </div>
+              </div>
+
           </div>
 
-               {/*Image of orange juice*/}
-                <div className='FeatureSecOrangeGlass'>
-                  <img src={OrangeContainer} alt='OrangeJuiceGlass' />
-                </div>
-    </div>
-                
-    </>
+          {/*Image of orange juice*/}
+           <div className='FeatureSecOrangeGlass'>
+             <img src={OrangeContainer} alt='OrangeJuiceGlass' />
+           </div>
+
+    </div>          
+ </>
   )
 }
