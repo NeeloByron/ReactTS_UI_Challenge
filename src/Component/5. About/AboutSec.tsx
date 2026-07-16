a39d11cc70e6cedd180cbd0d826ad8bf15277209
NeeloByron <byronnkhuna@gmail.com>
2026-07-16 15:25:53 +0200

    Update

--- src/Component/5. About/AboutSec.tsx
+++ src/Component/5. About/AboutSec.tsx
@@ -4,35 +4,35 @@ import './aboutSec.Module.css'
 
 export const AboutSec = () => {
   return (
-     <>
-        {/*Main section*/}
-        <div className='aboutSection'> 
-          <div className='aboutText'>
-             <p>Just imagine seeds and summer in gell sunshine.</p>
-           </div>
-           
-           {/*contains the two cards*/}
-           <div className='containerCard'>
+    <>
+      {/*Main section*/}
+      <div className='aboutSection'>
+        <div className='aboutText'>
+          <p>Just imagine seeds and summer in gell sunshine.</p>
+        </div>
 
-             {/*Image for the middle card holding strewberries*/}
-              <div className='aboutImageCard'>
-                <img src={strewBerryImg} alt="Strewberries" />
-              </div>
+        {/*contains the two cards*/}
+        <div className='containerCard'>
 
-              {/*the which background card*/}
-             <div className='cardBox'>
-               <h2>EBEE WOUS </h2>
-               <h1>BENEFIT.</h1>
-               <p className='Wotv'>Wotv & verd. Pasafafadd aafafa</p>
-               <hr className='diverOne'/>
-               <p className='Ruoedll'>ruocedll condition</p>
-               <hr className='diverTwo'/>
-               <Button btnText='HEAD MORE' onClick={() => alert('Head more to other things on our Restaurant')} />
-             </div>   
-              
-          </div> 
+          {/*Image for the middle card holding strewberries*/}
+          <div className='aboutImageCard'>
+            <img src={strewBerryImg} alt="Strewberries" />
+          </div>
+
+          {/*the which background card*/}
+          <div className='cardBox'>
+            <h2>EBEE WOUS </h2>
+            <h1>BENEFIT.</h1>
+            <p className='Wotv'>Wotv & verd. Pasafafadd aafafa</p>
+            <hr className='diverOne' />
+            <p className='Ruoedll'>ruocedll condition</p>
+            <hr className='diverTwo' />
+            <Button btnText='HEAD MORE' onClick={() => alert('Head more to other things on our Restaurant')} />
+          </div>
+
+        </div>
 
       </div>
     </>
-      )
+  )
 }
